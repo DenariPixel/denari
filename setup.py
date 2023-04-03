c3e44282e3b6c1f570b94666793e3b98da09fb15
DenariAnalytics <fid_kk@proton.me>
2023-04-03 19:06:26 +0100

    bug fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='denari',
-    version='1.0.34',
+    version='1.0.35',
     description='DenariAnalytics OpenSource Business and Tax Tools',
     author='Fadil Karim',
     author_email='insights@denarianalytics.com',
