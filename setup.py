9ad88f1e79155959707e5ec2e39ff12c75887e7c
Fadil Karim <fid_kk@proton.me>
2024-01-16 17:01:28 +0000

    pypi automation added

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='denari',
-    version='1.0.45',
+    version='1.0.47',
     description='DenariAnalytics OpenSource Business and Tax Tools',
     author='Fadil Karim',
-    author_email='insights@denarianalytics.com',
+    author_email='fid_kk@proton.me',
     packages=find_packages(),
     install_requires=[
         'pandas',
